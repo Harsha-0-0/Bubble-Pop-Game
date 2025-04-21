118a0f0fb5e38cbe791f9ba5ce794599d37167d3
Harsha <harshiismyname@gmail.com>
2025-04-21 18:50:19 +1000

    Initial Commit

---
+++ Bubble Pop Game/Bubble_Pop_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  Bubble_Pop_GameApp.swift
+//  Bubble Pop Game
+//
+//  Created by Harshi on 21/04/25.
+//
+
+import SwiftUI
+
+@main
+struct Bubble_Pop_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
